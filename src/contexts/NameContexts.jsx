dd16104cb5ddc436f4768d893f6b4b4a127d429f
iagoliziero <iagolizierox14@gmail.com>
2025-03-06 14:36:50 -0300

    fix: restore deleted file NameContexts

---
+++ src/contexts/NameContexts.jsx
@@ -0,0 +1,16 @@
+import { createContext, useState } from "react";
+
+export const NameContext = createContext()
+
+function NameProvider({children}) {
+    const [name, setName] = useState('')
+
+    return (
+        <NameContext.Provider value={{name, setName}}>
+            {children}
+        </NameContext.Provider>
+    )
+
+}
+
+export default NameProvider;
