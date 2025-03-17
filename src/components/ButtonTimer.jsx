571a79eb4010c5bac12171115f581f815427ce87
iagoliziero <iagolizierox14@gmail.com>
2025-03-17 19:18:46 -0300

    feat: create ButtonTimer component

---
+++ src/components/ButtonTimer.jsx
@@ -0,0 +1,11 @@
+const ButtonTimer = (props) => {
+    return (
+        <button
+                    {...props}
+                    className="w-14 h-14 xxl:w-16 xxl:h-16 rounded-full items-center bg-darker flex justify-center text-whiteMain hover:scale-105 transition-all"> {props.children}
+                    </button>
+    )
+}
+
+
+export default ButtonTimer;
