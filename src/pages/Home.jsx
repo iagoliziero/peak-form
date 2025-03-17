4f59ed6162192706f5a25b1dee2ae8763c287efe
iagoliziero <iagolizierox14@gmail.com>
2025-03-17 19:13:35 -0300

    style: update image size and site dimensions

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,25 +2,30 @@ import { CircleUserRound } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 import H1 from "../components/H1";
 
+
 function Home() {
   const navigate = useNavigate()
 
+
   return (
-    <div className="h-screen bg-darker bg-cover bg-center bg-no-repeat flex flex-col items-center gap-12 lg:justify-center overflow-scroll no-scrollbar">
-      <div className=" flex flex-col justify-center xxl:mt-20 3xl:mt-0 gap-8">
-      <div>
-          <button 
-          onClick={() => navigate(`/signin`)}
-          className="w-10 h-10 top-[5rem] md:left-[21.875rem] lg:left-[34.375rem] rounded-full items-center bg-yellowMain hover:bg-orange flex justify-center relative left-52 "> <CircleUserRound size={30} /> </button>
-          </div>
+    <div className="h-screen xl:h-full xxl:h-screen bg-darker bg-cover bg-center bg-no-repeat flex flex-col items-center gap-12 lg:justify-center overflow-scroll no-scrollbar">
+      <div className=" flex flex-col justify-center  3xl:mt-0 gap-8">
+        <div className="mt-">
+          <button
+            onClick={() => navigate(`/signin`)}
+            className="w-10 h-10 top-[5rem] md:left-[21.875rem] lg:left-[34.375rem] rounded-full items-center bg-yellowMain hover:bg-orange flex justify-center relative left-52 "> <CircleUserRound size={30} /> </button>
+        </div>
         <span className="flex items-center justify-center">
           <H1>
-            
+
+
             Bem vindo ao
           </H1>
-          
+
+
         </span>
-        
+
+
         <span className="flex flex-col items-center">
           <img
             className="w-[9rem] md:w-[11rem] xl:w-[12rem]"
@@ -32,7 +37,7 @@ function Home() {
       <div className="flex flex-col gap-8 lg:flex-row">
         <div className="flex flex-col gap-4">
           <img
-            className="w-[21.875rem] lg:w-[18.75rem] rounded-2xl"
+            className="w-[21.875rem] lg:w-[14.75rem] xxl:w-[18rem] rounded-2xl"
             src="/src/public/image1.png"
             alt=""
           />
@@ -43,7 +48,7 @@ function Home() {
         </div>
         <div className="flex flex-col gap-4">
           <img
-            className="w-[21.875rem] lg:w-[18.75rem] rounded-2xl"
+            className="w-[21.875rem] xxl:w-[18rem] lg:w-[14.75rem] rounded-2xl"
             src="/src/public/image2.png"
             alt=""
           />
@@ -53,7 +58,7 @@ function Home() {
         </div>
         <div className="flex flex-col gap-4">
           <img
-            className="w-[21.875rem] lg:w-[18.75rem] rounded-2xl"
+            className="w-[21.875rem] xxl:w-[18rem] lg:w-[14.75rem] rounded-2xl"
             src="/src/public/image3.png"
             alt=""
           />
@@ -61,19 +66,21 @@ function Home() {
             Organização, <br /> disciplina e resultado.
           </p>
         </div>
-        
+
+
       </div>
-      <div className="flex flex-col sticky bottom-0 lg:relative mt-10 items-center md:items-center">
-            <button
-            onClick={() => navigate(`/nome`)}
-              className="w-[18.75rem] md:w-[22.5rem] h-[3.75rem]
+      <div className="flex flex-col sticky bottom-0 lg:relative items-center md:items-center">
+        <button
+          onClick={() => navigate(`/nome`)}
+          className="w-[18rem] md:w-[16rem] xxl:h-[3rem] h-[3.75rem]
                     bg-yellowMain mb-12 text-2xl rounded-lg hover:bg-orange hover:scale-105 transition-all duration-[400ms] font-semibold"
-            >
-              Continuar
-            </button>
-          </div>
+        >
+          Continuar
+        </button>
+      </div>
     </div>
   );
 }
+
 
 export default Home;
