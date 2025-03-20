95b1d889bd92e0931f59ec55c92a4de771085e48
iagoliziero <iagolizierox14@gmail.com>
2025-03-19 23:03:21 -0300

    feat: install React input library and implement input component

--- src/pages/InfoUserPage.jsx
+++ src/pages/InfoUserPage.jsx
@@ -1,55 +1,69 @@
-import { useForm } from "react-hook-form";
-import Calendar from "../components/Calendar";
+import {useForm } from "react-hook-form";
 import Gender from "../components/Gender";
 import LocalInput from "../components/LocalInput";
-import { Navigate, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import Logo from "../components/Logo";
 import H1 from "../components/H1";
 import ButtonNext from "../components/ButtonNext";
 import ButtonBack from "../components/ButtonBack";
 
-function InfoUserPage() {
+import ReactInputMask from "react-input-mask";
 
-  const navigate = useNavigate()
+function InfoUserPage() {
+  
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    alert(JSON.stringify(data));
+  
+    navigate(`/login`);
+  };
+
+  const navigate = useNavigate();
   return (
     <div className="h-screen bg-darker bg-cover bg-center bg-no-repeat flex flex-col items-center gap-12 overflow-scroll no-scrollbar md:justify-center lg:justify-start">
       <Logo />
       <div className="flex-col flex gap-5">
         <div className="mt-5 flex flex-col gap-5">
-        <H1> 
-        Qual é o seu gênero?
-        </H1>
+          <H1>Qual é o seu gênero?</H1>
 
           <Gender />
-
         </div>
         <div>
-          <div className=" flex flex-col gap-5">
+          <div className=" flex flex-col gap-5 ">
             {" "}
-            <H1>
-            Quando você nasceu?
-              </H1>
-
-            <Calendar  />
-
+            <H1>Quando você nasceu?</H1>
+            <ReactInputMask
+              {...register("date", { required: true })}
+              onChange={(e) => setProfile(e.target.value)}
+              className={`mt-5 w-[21.5rem] md:w-[20rem] h-[3.3rem] rounded-md bg-darker border border-gray text-3xl p-4 text-whiteMain focus:outline-none focus:ring-3 focus:border-yellowMain ${
+                errors?.date ? "border-lightRed" : "focus:border-yellowMain"
+              }`}
+              mask="99/99/9999"
+              placeholder="Data de nascimento"
+              maskChar=""
+            />
+            
           </div>
-          <div className=" flex justify-center items-center flex-col gap-5">
-          <H1> 
-          Onde você mora?
-          </H1>
+          {errors?.date?.type === "required" && (
+              <p className="text-lightRed mx-4 mt-1">
+                {" "}
+                Digite sua data de nascimento.{" "}
+              </p>)}
+          <div className=" flex justify-center items-center flex-col gap-5 mt-5">
+            <H1>Onde você mora?</H1>
 
             <LocalInput />
-
           </div>
         </div>
       </div>
       <div className="mb-5 flex flex-row lg:relative mt-8  items-center md:items-center gap-6">
-        <ButtonBack 
-        onClick={() => navigate(-1)}>
-          Voltar
-        </ButtonBack>
-        <ButtonNext 
-        onClick={() => navigate(`/login`)}>
+        <ButtonBack onClick={() => navigate(-1)}>Voltar</ButtonBack>
+        <ButtonNext onClick={() => handleSubmit(onSubmit)()}>
           Continuar
         </ButtonNext>
       </div>
