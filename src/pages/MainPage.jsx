e8520a8fc6e911e124879586910537f2f6ce2fa4
iagoliziero <iagolizierox14@gmail.com>
2025-03-24 09:13:53 -0300

    feat: insert icons from mainPage

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,4 +1,4 @@
-import { Info, Pencil, Timer, Trash2 } from "lucide-react";
+import { BicepsFlexed, Dumbbell, ListOrdered, Pencil, Repeat, Timer, Trash2 } from "lucide-react";
 import DropdownMenu from "../components/DropDownMenu";
 import { useContext } from "react";
 import { ExerciseContext } from "../contexts/ExerciseContext";
@@ -65,7 +65,7 @@ function MainPage() {
         >
           {exercise.map((exer) => (
             <div key={exer.id} className="snap-center flex-shrink-0">
-              <div className="w-[17rem] h-[26rem] 3xl:w-[20rem] bg-yellowMain rounded-lg flex-col">
+              <div className="w-[17rem] h-[24rem] 3xl:w-[20rem] bg-yellowMain rounded-lg flex-col">
                 <div className="flex items-center justify-between p-4">
                   <h1 className="text-start text-2xl text-darker font-semibold">
                     {exer.title}
@@ -73,24 +73,44 @@ function MainPage() {
                   <InfoCard exercise={exer}/>
                 </div>
                 <div className="flex flex-col justify-center items-center">
-                  <div className="w-[16rem] h-[21.5rem] 3xl:w-[19rem] bg-whiteMain rounded-md flex-col">
+                  <div className="w-[16rem] h-[19.5rem] 3xl:w-[19rem] bg-whiteMain rounded-md flex-col">
                     <div className="flex p-6">
-                      <div className="flex flex-col gap-6">
+                      <div className="flex flex-col gap-5">
+                      <div className="flex gap-3 items-center">
+                     <span className="text-xl text-orange font-bold">
+                      <ListOrdered />
+                      </span>
                       <h2 className="text-xl text-darker font-bold">
-                        Quantidade de séries: {""} <br className="3xl:hidden" /> {exer.numberSeries}
+                      {exer.numberSeries}
                       </h2>
+                     </div>
+                      <div className="flex gap-3 items-center">
+                     <span className="text-xl text-orange font-bold">
+                     <Repeat />
+                      </span>
                       <h2 className="text-xl text-darker font-bold">
-                        Repetições: {exer.repetitions}
+                      {exer.repetitions}
                       </h2>
+                     </div>
+                     <div className="flex gap-3 items-center">
+                     <span className="text-xl text-orange font-bold">
+                      <Dumbbell />
+                      </span>
                       <h2 className="text-xl text-darker font-bold">
-                        Técnicas avançadas: <br /> {exer.advancedTechnique}
+                      {exer.advancedTechnique}
                       </h2>
+                     </div>
+                     <div className="flex gap-3 items-center">
+                     <span className="text-xl text-orange font-bold">
+                      <BicepsFlexed />
+                      </span>
                       <h2 className="text-xl text-darker font-bold">
-                        Intensidade: {exer.intensity}
+                      {exer.intensity}
                       </h2>
+                     </div>
                       </div>
                     </div>
-                    <div className="flex justify-center items-center flex-grow gap-3 3xl:gap-5 relative bottom-1 3xl:bottom-0">
+                    <div className="flex justify-center items-center flex-grow gap-3 3xl:gap-5 relative top-3 3xl:bottom-0">
                       {/* Container dos botões */}
                       <button className="w-16 h-12 3xl:w-18 3xl:h-14 bg-yellowMain rounded-md hover:scale-105 transition-all text-darker flex justify-center items-center">
                         <Pencil size={34} strokeWidth={2} />
