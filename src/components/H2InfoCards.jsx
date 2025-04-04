f249ab4f627740244e65d88b202ca3df72edc338
iagoliziero <iagolizierox14@gmail.com>
2025-04-03 21:53:07 -0300

    feat: create H2InfoCards and H2Bold component

--- src/components/H2InfoCards.jsx
+++ src/components/H2InfoCards.jsx
@@ -1,6 +1,6 @@
 const H2InfoCards = ({children}) => {
     return ( 
-        <h2 className="text-xl text-orange  relative left-1 font-bold ">
+        <h2 className="text-xl lg:text-2xl text-orange relative left-1 font-bold ">
            {children}
         </h2>
      );
