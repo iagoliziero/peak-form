2975c56f60013d2517fe41007d5dcc4f73f0d807
iagoliziero <iagolizierox14@gmail.com>
2025-06-10 12:28:09 -0300

    fix: update API base URL to production server

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://backend-peakform.onrender.com',
 });
 
 api.interceptors.request.use((config) => {
